6678d22c087f2d7a99212118dee47f621d82e6e2
lanyeeee <lanyeeeeee@gmail.com>
2025-03-09 06:27:39 +0800

    feat: 后端支持保存配置文件

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,8 +1,12 @@
 use parking_lot::RwLock;
-use tauri::State;
+use tauri::{AppHandle, State};
 
 use crate::{
-    config::Config, errors::{CommandError, CommandResult}, types::UserProfile, wnacg_client::WnacgClient
+    config::Config,
+    errors::{CommandError, CommandResult},
+    logger,
+    types::UserProfile,
+    wnacg_client::WnacgClient,
 };
 
 #[tauri::command]
@@ -18,6 +22,43 @@ pub fn get_config(config: tauri::State<RwLock<Config>>) -> Config {
     let config = config.read().clone();
     tracing::debug!("获取配置成功");
     config
+}
+
+#[tauri::command(async)]
+#[specta::specta]
+#[allow(clippy::needless_pass_by_value)]
+pub fn save_config(
+    app: AppHandle,
+    config_state: State<RwLock<Config>>,
+    config: Config,
+) -> CommandResult<()> {
+    let enable_file_logger = config.enable_file_logger;
+    let enable_file_logger_changed = config_state
+        .read()
+        .enable_file_logger
+        .ne(&enable_file_logger);
+
+    {
+        // 包裹在大括号中，以便自动释放写锁
+        let mut config_state = config_state.write();
+        *config_state = config;
+        config_state
+            .save(&app)
+            .map_err(|err| CommandError::from("保存配置失败", err))?;
+        tracing::debug!("保存配置成功");
+    }
+
+    if enable_file_logger_changed {
+        if enable_file_logger {
+            logger::reload_file_logger()
+                .map_err(|err| CommandError::from("重新加载文件日志失败", err))?;
+        } else {
+            logger::disable_file_logger()
+                .map_err(|err| CommandError::from("禁用文件日志失败", err))?;
+        }
+    }
+
+    Ok(())
 }
 
 #[tauri::command(async)]
